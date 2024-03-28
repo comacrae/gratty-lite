901092717d7cf83dfba504b2d140f702cd29d15d
comacrae <comacrae1995@gmail.com>
2024-03-27 21:05:47 -0500

    added login form

--- gratty-lite/src/routes/login.jsx
+++ gratty-lite/src/routes/login.jsx
@@ -1,9 +1,53 @@
 import { Form, redirect } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import GrattyNavbar from "../components/navbar";
 
 export default function Login() {
-  return <>Login</>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useAuth();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (username === "user" && password === "password") {
+      await login({ username });
+    } else {
+      alert("Invalid username or password");
+    }
+  };
+
+  return (
+    <Container>
+      <GrattyNavbar />
+
+      <div>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </Container>
+  );
 }
